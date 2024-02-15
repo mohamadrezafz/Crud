237b57b64991d7b17c957f6debe20c04894fd2f8
m.firooz <m.firooz@tosansoha.com>
2024-02-15 22:08:00 +0330

    create structure of domain add - common - entities - events

---
+++ Crud.Domain/Common/BaseEvent.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Crud.Domain.Common;
+
+public interface IHasBaseEvent
+{
+    public List<BaseEvent> BaseEvents { get; set; }
+}
+public class BaseEvent
+{
+    public bool IsPublish { get; set; }
+    public DateTime DateTimeOccurred { get; set; } = DateTime.Now;
+}
+
---
+++ Crud.Domain/Entities/Customer.cs
@@ -0,0 +1,16 @@
+﻿
+using Crud.Domain.Common;
+
+namespace Crud.Domain.Entities;
+
+public class Customer :BaseEntity
+{
+    public string FirstName { get; set; } = default!;
+    public string LastName { get; set; } = default!;
+    public DateTime DateOfBirth { get; set; }
+    public string PhoneNumber { get; set; } = default!;
+    public string Email { get; set; } = default!;
+    public string BankAccountNumber { get; set; } = default!;
+    public List<BaseEvent> BaseEvents { get; set; } = default!;
+}
+
